d440fb282c439993d9bd87c32ba4288d02226452
Difz25x <177396246+Difz25x@users.noreply.github.com>
2025-11-27 20:47:31 +0700

    Create luarmor-workink.user.js

---
+++ luarmor-workink.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Luarmor & Work.ink Bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0.0
+// @description  Bypass Volcano and Work.ink links with unified panel
+// @author       Difz25x
+// @match        https://ads.luarmor.net/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @downloadURL  https://github.com/Difz25x/volcano-bypass/raw/refs/heads/main/luarmor-workink.user.js
+// @updateURL    https://github.com/Difz25x/volcano-bypass/raw/refs/heads/main/luarmor-workink.user.js
+// @require      https://github.com/Difz25x/volcano-bypass/raw/refs/heads/main/luarmor-workink.js
+// ==/UserScript==
